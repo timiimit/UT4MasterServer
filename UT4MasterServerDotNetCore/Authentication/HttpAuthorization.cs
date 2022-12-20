2980d5cb005594d0f1adc18e6776d001433e3ff5
timiimit <timi.korda@gmail.com>
2022-12-20 10:56:11 +0100

    Fix more indentations

--- UT4MasterServerDotNetCore/Authentication/HttpAuthorization.cs
+++ UT4MasterServerDotNetCore/Authentication/HttpAuthorization.cs
@@ -3,30 +3,30 @@
 namespace UT4MasterServer.Authorization;
 
 public class HttpAuthorization
-    {
-        public string Type { get; set; } = string.Empty;
-        public string Value { get; set; } = string.Empty;
+{
+	public string Type { get; set; } = string.Empty;
+	public string Value { get; set; } = string.Empty;
 
-        public bool IsBearer { get => Type == "bearer"; }
-        public bool IsBasic { get => Type == "basic"; }
+	public bool IsBearer { get => Type == "bearer"; }
+	public bool IsBasic { get => Type == "basic"; }
 
-        public HttpAuthorization(HttpListenerRequest request) : this(request.Headers["Authorization"])
-        {
-        }
+	public HttpAuthorization(HttpListenerRequest request) : this(request.Headers["Authorization"])
+	{
+	}
 
-        public HttpAuthorization(string? authorizationHeader)
-        {
-            if (authorizationHeader == null)
-                return;
+	public HttpAuthorization(string? authorizationHeader)
+	{
+		if (authorizationHeader == null)
+			return;
 
-            int space = authorizationHeader.IndexOf(' ');
-            if (space == -1)
-            {
-                Value = authorizationHeader;
-                return;
-            }
+		int space = authorizationHeader.IndexOf(' ');
+		if (space == -1)
+		{
+			Value = authorizationHeader;
+			return;
+		}
 
-            Type = authorizationHeader.Substring(0, space);
-            Value = authorizationHeader.Substring(space + 1);
-        }
-    }
+		Type = authorizationHeader.Substring(0, space);
+		Value = authorizationHeader.Substring(space + 1);
+	}
+}
