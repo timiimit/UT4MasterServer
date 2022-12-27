1bc01e55063eed965e58ef96f680258b4fb5cf70
timiimit <timi.korda@gmail.com>
2022-12-27 21:48:33 +0100

    Move helper methods to extensions class

--- UT4MasterServer/Controllers/JsonAPIController.cs
+++ UT4MasterServer/Controllers/JsonAPIController.cs
@@ -75,27 +75,4 @@ public class JsonAPIController : ControllerBase
 	{
 		return new JsonResult(content, new JsonSerializerOptions() { Converters = { new EpicIDJsonConverter() } }) { StatusCode = status };
 	}
-
-
-	[NonAction]
-	public async Task<string> ReadBodyAsStringAsync(int maxLength)
-	{
-		// this method doesn't really belong in this class
-		// but we don't have any other commonly accessible location
-
-		return Encoding.UTF8.GetString(await ReadBodyAsBytesAsync(maxLength));
-	}
-
-	[NonAction]
-	public async Task<byte[]> ReadBodyAsBytesAsync(int maxLength)
-	{
-		// this method doesn't really belong in this class
-		// but we don't have any other commonly accessible location
-
-		byte[] body = new byte[maxLength];
-		var bodyLength = await Request.Body.ReadAsync(body);
-		Array.Resize(ref body, bodyLength);
-
-		return body;
-	}
 }
