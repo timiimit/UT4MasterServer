d0700f2a52e0a907a1f4988991f585fe94c6a485
timiimit <timi.korda@gmail.com>
2023-01-07 21:51:32 +0100

    Change ShowMyIP to ShowMyInfo and display more info

--- UT4MasterServer/Controllers/CustomAPIController.cs
+++ UT4MasterServer/Controllers/CustomAPIController.cs
@@ -1,6 +1,8 @@
 
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System.Text;
 using UT4MasterServer.Models;
 using UT4MasterServer.Services;
 
@@ -16,11 +18,37 @@ public class CustomAPIController : JsonAPIController
 		this.configuration = configuration;
 	}
 
-	[HttpGet("ShowMyIP")]
-	public IActionResult ShowMyIP()
+	[HttpGet("ShowMyInfo")]
+	public IActionResult ShowMyInfo()
 	{
 		var ip = GetClientIP(configuration);
 
-		return Content($"Received IP: {HttpContext.Connection.RemoteIpAddress}\nProxy says it's actually: {ip}");
+		StringBuilder sb = new StringBuilder();
+
+		sb.AppendLine($"Direct IP Address: {HttpContext.Connection.RemoteIpAddress}");
+		sb.AppendLine($"Guessed IP Address: {ip}");
+
+		sb.AppendLine();
+		sb.AppendLine($"HTTP Method: {HttpContext.Request.Method}");
+		sb.AppendLine($"Display URL: {HttpContext.Request.GetDisplayUrl()}");
+
+		sb.AppendLine();
+		sb.AppendLine("Query URL Parameters:");
+		foreach (var parameter in HttpContext.Request.Query)
+		{
+			sb.AppendLine($"{parameter.Key}=");
+		}
+
+		sb.AppendLine();
+		sb.AppendLine("Request Headers:");
+		foreach (var header in HttpContext.Request.Headers)
+		{
+			foreach (var headerInstance in header.Value)
+			{
+				sb.AppendLine($"{header.Key}: {headerInstance}");
+			}
+		}
+
+		return Content(sb.ToString());
 	}
 }
