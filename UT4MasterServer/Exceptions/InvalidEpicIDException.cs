2604293baedba8e65ffc9cddc0f575a9f0d58b06
Belmir Patkovic <belmirp@hotmail.com>
2023-01-13 20:41:39 +0100

    Added custom exception for EpicID validation

---
+++ UT4MasterServer/Exceptions/InvalidEpicIDException.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace UT4MasterServer.Exceptions;
+
+[Serializable]
+public class InvalidEpicIDException : Exception
+{
+	public InvalidEpicIDException(string message) : base(message) { }
+
+	protected InvalidEpicIDException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
+}
