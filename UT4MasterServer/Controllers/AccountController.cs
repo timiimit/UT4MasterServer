766af8e5c5014187b8382ad3a7ab9c95fbfd0451
Counter Rebel <habib.m.moflehi@gmail.com>
2023-01-13 18:15:31 -0500

    Moved the check if email is null check to before the Regex match check

--- UT4MasterServer/Controllers/AccountController.cs
+++ UT4MasterServer/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
 using UT4MasterServer.Authentication;
 using UT4MasterServer.Models;
 using UT4MasterServer.Models.Requests;
@@ -182,6 +183,11 @@ public class AccountController : JsonAPIController
 		}
 
 		var email = await accountService.GetAccountEmailAsync(request.Email);
+		if (email != null)
+		{
+			logger.LogInformation($"Could not register duplicate email: {request.Email}");
+			return Conflict("Email already exists");
+		}
 
 		Regex regex = new Regex(@"^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])");
 		Match match = regex.Match(request.Email);
@@ -189,11 +195,6 @@ public class AccountController : JsonAPIController
 		{
 			logger.LogInformation($"Entered an incorrect email format: {request.Email}");
 			return Conflict("You have entered and invalid email address");
-		}
-		if (email != null)
-		{
-			logger.LogInformation($"Could not register duplicate email: {request.Email}");
-			return Conflict("Email already exists");
 		}
 
 		await accountService.CreateAccountAsync(request.Username, request.Email, request.Password); // TODO: this cannot fail?
