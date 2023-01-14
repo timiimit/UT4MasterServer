bb6f3c0921406e6059789668b9f7b7cbf005f934
timiimit <timi.korda@gmail.com>
2023-01-14 15:21:36 +0100

    Fix indentation

--- UT4MasterServer.Web/src/pages/Instructions/UT4UU.vue
+++ UT4MasterServer.Web/src/pages/Instructions/UT4UU.vue
@@ -2,17 +2,17 @@
     <h1>Instructions for UT4 with UT4UU</h1>
     <p>Use these instructions if you have the UT4UU plugin installed.</p>
     <ol>
-        <li>Open the game</li>
-        <li>Open console (~)</li>
-        <li>Enter command <span class="code">uu settings</span></li>
-        <li>Go to Other tab</li>
-        <li>Scroll to the bottom</li>
-        <li>In the Master Server Domain section, enter <span class="code">master-ut4.timiimit.com</span> into the textbox</li>
-        <li>Press APPLY in bottom right of the window</li>
-        <li>Close the game</li>
-        <li>Start game</li>
-        <li>Get Auth Code and use it in UT4UU's login window</li>
-        <li>Use the newly added account to log in</li>
+      <li>Open the game</li>
+      <li>Open console (~)</li>
+      <li>Enter command <span class="code">uu settings</span></li>
+      <li>Go to Other tab</li>
+      <li>Scroll to the bottom</li>
+      <li>In the Master Server Domain section, enter <span class="code">master-ut4.timiimit.com</span> into the textbox</li>
+      <li>Press APPLY in bottom right of the window</li>
+      <li>Close the game</li>
+      <li>Start game</li>
+      <li>Get Auth Code and use it in UT4UU's login window</li>
+      <li>Use the newly added account to log in</li>
     </ol>
 </template>
 <script setup lang="ts">
