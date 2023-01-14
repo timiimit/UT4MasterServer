ab3bd674c5845e442117faac1488cc5652700654
Casey Williams <caseyswilliams@gmail.com>
2023-01-14 18:41:28 -0500

    Navigation bug fix

--- UT4MasterServer.Web/src/pages/Instructions/UT4UU.vue
+++ UT4MasterServer.Web/src/pages/Instructions/UT4UU.vue
@@ -1,21 +1,22 @@
 <template>
-    <h1>Instructions for UT4 with UT4UU</h1>
-    <p>Use these instructions if you have the UT4UU plugin installed.</p>
-    <ol>
-      <li>Open the game</li>
-      <li>Open console (~)</li>
-      <li>Enter command <span class="code">uu settings</span></li>
-      <li>Go to Other tab</li>
-      <li>Scroll to the bottom</li>
-      <li>In the Master Server Domain section, enter <span class="code">master-ut4.timiimit.com</span> into the textbox</li>
-      <li>Press APPLY in bottom right of the window</li>
-      <li>Close the game</li>
-      <li>Start game</li>
-      <li>Get Auth Code and use it in UT4UU's login window</li>
-      <li>Use the newly added account to log in</li>
-    </ol>
+  <h1>Instructions for UT4 with UT4UU</h1>
+  <p>Use these instructions if you have the UT4UU plugin installed.</p>
+  <ol>
+    <li>Open the game</li>
+    <li>Open console (~)</li>
+    <li>Enter command <span class="code">uu settings</span></li>
+    <li>Go to Other tab</li>
+    <li>Scroll to the bottom</li>
+    <li>In the Master Server Domain section, enter
+      <CodeBlock>master-ut4.timiimit.com</CodeBlock>
+    </li>
+    <li>Press APPLY in bottom right of the window</li>
+    <li>Close the game</li>
+    <li>Start game</li>
+    <li>Get Auth Code and use it in UT4UU's login window</li>
+    <li>Use the newly added account to log in</li>
+  </ol>
 </template>
 <script setup lang="ts">
 import CodeBlock from '../../components/CodeBlock.vue';
 </script>
-
