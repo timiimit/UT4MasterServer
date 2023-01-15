d25050e2575214c696d20d4229c6651b4f068041
Belmir Patkovic <belmirp@hotmail.com>
2023-01-15 13:21:40 +0100

    Added ID field to InvalidEpicIDException

--- UT4MasterServer/Exceptions/InvalidEpicIDException.cs
+++ UT4MasterServer/Exceptions/InvalidEpicIDException.cs
@@ -5,26 +5,32 @@ namespace UT4MasterServer.Exceptions;
 [Serializable]
 public class InvalidEpicIDException : Exception
 {
+	private string _id { get; set; }
+	public string ID { get { return _id; } }
+
 	private int _numericErrorCode { get; set; }
 	public int NumericErrorCode { get { return _numericErrorCode; } }
 
 	private string _errorCode { get; set; }
 	public string ErrorCode { get { return _errorCode; } }
 
-	public InvalidEpicIDException(string message, int numericErrorCode, string errorCode) : base(message)
+	public InvalidEpicIDException(string message, string id, int numericErrorCode, string errorCode) : base(message)
 	{
+		_id = id;
 		_numericErrorCode = numericErrorCode;
 		_errorCode = errorCode;
 	}
 
-	public InvalidEpicIDException(string message, int numericErrorCode, string errorCode, Exception innerException) : base(message, innerException)
+	public InvalidEpicIDException(string message, string id, int numericErrorCode, string errorCode, Exception innerException) : base(message, innerException)
 	{
+		_id = id;
 		_numericErrorCode = numericErrorCode;
 		_errorCode = errorCode;
 	}
 
 	protected InvalidEpicIDException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext)
 	{
+		_id = serializationInfo.GetString(nameof(ID)) ?? string.Empty;
 		_numericErrorCode = serializationInfo.GetInt32(nameof(NumericErrorCode));
 		_errorCode = serializationInfo.GetString(nameof(ErrorCode)) ?? string.Empty;
 	}
@@ -36,6 +42,7 @@ public class InvalidEpicIDException : Exception
 			throw new ArgumentNullException(nameof(info));
 		}
 
+		info.AddValue(nameof(ID), ID);
 		info.AddValue(nameof(NumericErrorCode), NumericErrorCode);
 		info.AddValue(nameof(ErrorCode), ErrorCode);
 
