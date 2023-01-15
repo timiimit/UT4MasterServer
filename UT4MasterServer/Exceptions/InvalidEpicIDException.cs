a49e00a452a2353466c5815f6f8e28313071a53f
Belmir Patkovic <belmirp@hotmail.com>
2023-01-15 12:53:00 +0100

    Expanded InvalidEpicIDException with NumericErrorCode and ErrorCode fields

--- UT4MasterServer/Exceptions/InvalidEpicIDException.cs
+++ UT4MasterServer/Exceptions/InvalidEpicIDException.cs
@@ -5,7 +5,40 @@ namespace UT4MasterServer.Exceptions;
 [Serializable]
 public class InvalidEpicIDException : Exception
 {
-	public InvalidEpicIDException(string message) : base(message) { }
+	private int _numericErrorCode { get; set; }
+	public int NumericErrorCode { get { return _numericErrorCode; } }
 
-	protected InvalidEpicIDException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
+	private string _errorCode { get; set; }
+	public string ErrorCode { get { return _errorCode; } }
+
+	public InvalidEpicIDException(string message, int numericErrorCode, string errorCode) : base(message)
+	{
+		_numericErrorCode = numericErrorCode;
+		_errorCode = errorCode;
+	}
+
+	public InvalidEpicIDException(string message, int numericErrorCode, string errorCode, Exception innerException) : base(message, innerException)
+	{
+		_numericErrorCode = numericErrorCode;
+		_errorCode = errorCode;
+	}
+
+	protected InvalidEpicIDException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext)
+	{
+		_numericErrorCode = serializationInfo.GetInt32(nameof(NumericErrorCode));
+		_errorCode = serializationInfo.GetString(nameof(ErrorCode)) ?? string.Empty;
+	}
+
+	public override void GetObjectData(SerializationInfo info, StreamingContext context)
+	{
+		if (info == null)
+		{
+			throw new ArgumentNullException(nameof(info));
+		}
+
+		info.AddValue(nameof(NumericErrorCode), NumericErrorCode);
+		info.AddValue(nameof(ErrorCode), ErrorCode);
+
+		base.GetObjectData(info, context);
+	}
 }
