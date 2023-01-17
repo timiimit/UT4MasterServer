d3249bcc2480f2ce79e7e559171a095aec819d66
Belmir Patkovic <belmirp@hotmail.com>
2023-01-17 23:26:30 +0100

    Changed error numeric code and string code for Invalid EpicID

--- UT4MasterServer/Other/EpicID.cs
+++ UT4MasterServer/Other/EpicID.cs
@@ -27,14 +27,14 @@ public struct EpicID : IComparable<EpicID>, IEquatable<EpicID>, IConvertible, IB
 	{
 		if (id.Length != 32)
 		{
-			throw new InvalidEpicIDException("ID needs to be 32 characters long.", id, 18009, "errors.com.epicgames.account.id_not_32_characters");
+			throw new InvalidEpicIDException("ID needs to be 32 characters long.", id, 18007, "errors.com.epicgames.modules.stats.invalid_account_id");
 		}
 
 		ID = id.ToLower();
 
 		if (!ID.IsHexString())
 		{
-			throw new InvalidEpicIDException("ID needs to be a hexadecimal string.", id, 18008, "errors.com.epicgames.account.id_not_hexadecimal");
+			throw new InvalidEpicIDException("ID needs to be a hexadecimal string.", id, 18007, "errors.com.epicgames.modules.stats.invalid_account_id");
 		}
 	}
 
