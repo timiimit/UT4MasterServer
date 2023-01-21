450f5f1492dcd566b6a8762e1bb6c06a25449ba8
timiimit <timi.korda@gmail.com>
2023-01-21 18:51:36 +0100

    Add TrustedGameServer and its db service

---
+++ UT4MasterServer/Services/TrustedGameServerService.cs
@@ -0,0 +1,25 @@
+using MongoDB.Driver;
+using UT4MasterServer.Models;
+using UT4MasterServer.Other;
+
+namespace UT4MasterServer.Services;
+
+public class TrustedGameServerService
+{
+	private readonly IMongoCollection<TrustedGameServer> collection;
+
+	public TrustedGameServerService(DatabaseContext dbContext)
+	{
+		collection = dbContext.Database.GetCollection<TrustedGameServer>("trustedservers");
+	}
+
+	public async Task<TrustedGameServer?> Get(EpicID id, EpicID secret)
+	{
+		var options = new FindOptions<TrustedGameServer>()
+		{
+			Limit = 1
+		};
+		var cursor = await collection.FindAsync(x => x.ID == id && x.Secret == secret, options);
+		return await cursor.SingleOrDefaultAsync();
+	}
+}
