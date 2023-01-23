735054c449af3446062cb31a41f5748fad32a0cc
timiimit <timi.korda@gmail.com>
2023-01-23 18:27:20 +0100

    Add missing operations to TrustedGameServers collection

--- UT4MasterServer/Services/TrustedGameServerService.cs
+++ UT4MasterServer/Services/TrustedGameServerService.cs
@@ -19,4 +19,24 @@ public class TrustedGameServerService
 		var cursor = await collection.FindAsync(x => x.ID == id, options);
 		return await cursor.SingleOrDefaultAsync();
 	}
+
+	public async Task UpdateAsync(TrustedGameServer server)
+	{
+		var options = new ReplaceOptions() { IsUpsert = true };
+		await collection.ReplaceOneAsync(x => x.ID == server.ID, server, options);
+	}
+
+	public async Task<List<TrustedGameServer>> ListAsync()
+	{
+		var result = await collection.FindAsync(x => true);
+		return await result.ToListAsync();
+	}
+
+	public async Task<bool?> RemoveAsync(EpicID id)
+	{
+		var result = await collection.DeleteOneAsync(x => x.ID == id);
+		if (!result.IsAcknowledged)
+			return null;
+		return result.DeletedCount > 0;
+	}
 }
