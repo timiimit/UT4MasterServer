b06fe05e28ace13b072dcb1f923a65bf8fa41589
timiimit <timi.korda@gmail.com>
2023-01-28 11:53:15 +0100

    Add StatID and Version to fake stats.json output

--- UT4MasterServer/Controllers/CloudStorageController.cs
+++ UT4MasterServer/Controllers/CloudStorageController.cs
@@ -100,13 +100,15 @@ public sealed class CloudStorageController : JsonAPIController
 
 			// Send a fake response in order to fix #109 (which is a game bug)
 			var playerName = "New Player";
+			var playerID = EpicID.Empty;
 			var account = await accountService.GetAccountAsync(accountID);
 			if (account != null)
 			{
 				playerName = account.Username;
+				playerID = account.ID;
 			}
 
-			file = new CloudFile() { RawContent = Encoding.UTF8.GetBytes($"{{\"PlayerName\":\"{playerName}\"}}") };
+			file = new CloudFile() { RawContent = Encoding.UTF8.GetBytes($"{{\"PlayerName\":\"{playerName}\",StatsID:\"{playerID}\",Version:0}}") };
 		}
 
 		if (isStatsFile)
