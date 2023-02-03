e4725c5c5cc94ce684ae924e41a68ebae0e2463a
Belmir Patkovic <belmirp@hotmail.com>
2023-02-03 18:19:20 +0100

    Added comments for rating endpoints

--- UT4MasterServer/Controllers/UnrealTournamentRatingsController.cs
+++ UT4MasterServer/Controllers/UnrealTournamentRatingsController.cs
@@ -55,6 +55,9 @@ public sealed class UnrealTournamentRatingController : JsonAPIController
 		return Json(result);
 	}
 
+	// THIS IS RETURNING SOME RESULTS
+	// REQUEST: GET /ut/api/game/v2/ratings/account/0b0f09b400854b9b98932dd9e5abe7c5/league/RankedDuelSkillRating 
+	// RESPONSE: {"tier":2,"division":0,"points":13,"isInPromotionSeries":false,"promotionMatchesAttempted":3,"promotionMatchesWon":3,"placementMatchesAttempted":10}
 	[HttpGet("account/{id}/league/{leagueName}")]
 	public IActionResult LeagueRating(string id, string leagueName)
 	{
@@ -68,6 +71,9 @@ public sealed class UnrealTournamentRatingController : JsonAPIController
 		return Ok(league);
 	}
 
+	// ONLY SENDING ONE PLAYER ALWAYS
+	// REQUEST:	 { "members": [ { "accountId": "0b0f09b400854b9b98932dd9e5abe7c5", "score": 0, "isBot": false }], "socialPartySize": 1 }
+	// RESPONSE: { "rating":1666 }
 	[HttpPost("team/elo/{ratingType}")]
 	public IActionResult JoinQuickplay(string ratingType, [FromBody] RatingTeam body)
 	{
