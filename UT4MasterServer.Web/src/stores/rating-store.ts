f654ec0174fa6dfcbb2437f2dda20611f45e371f
Belmir Patkovic <belmirp@hotmail.com>
2023-02-06 19:41:59 +0100

    Using const instead of let

--- UT4MasterServer.Web/src/stores/rating-store.ts
+++ UT4MasterServer.Web/src/stores/rating-store.ts
@@ -22,7 +22,11 @@ export const RatingStore = {
     try {
       _status.value = AsyncStatus.BUSY;
 
-      let response = await _ratingService.getRankings(ratingType, skip, limit);
+      const response = await _ratingService.getRankings(
+        ratingType,
+        skip,
+        limit
+      );
       _rankings.value = response.data;
       _rankingsCount.value = response.count;
       _status.value = AsyncStatus.OK;
