d7edf2216b05d7000332bedd340d4546072a4e56
Belmir Patkovic <belmirp@hotmail.com>
2023-02-16 17:46:36 +0100

    Refactored ApplicationStartupService

--- UT4MasterServer.Services/Hosted/ApplicationStartupService.cs
+++ UT4MasterServer.Services/Hosted/ApplicationStartupService.cs
@@ -1,50 +1,42 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using UT4MasterServer.Models.Settings;
 using UT4MasterServer.Services.Scoped;
 
-namespace UT4MasterServer.Services.Hosted
+namespace UT4MasterServer.Services.Hosted;
+
+public sealed class ApplicationStartupService : IHostedService
 {
-	public sealed class ApplicationStartupService : IHostedService
+	private readonly ILogger<ApplicationStartupService> logger;
+	private readonly IServiceProvider serviceProvider;
+
+	public ApplicationStartupService(ILogger<ApplicationStartupService> logger, IServiceProvider serviceProvider)
 	{
-		private readonly ILogger<ApplicationStartupService> logger;
-		private readonly AccountService accountService;
-		private readonly StatisticsService statisticsService;
-		private readonly CloudStorageService cloudStorageService;
-		private readonly ClientService clientService;
-		private readonly RatingsService ratingsService;
-
-		public ApplicationStartupService(
-			ILogger<ApplicationStartupService> logger,
-			ILogger<StatisticsService> statsLogger,
-			IOptions<ApplicationSettings> settings,
-			ILogger<CloudStorageService> cloudStorageLogger,
-			ILogger<RatingsService> ratingsLogger)
-		{
-			this.logger = logger;
-			var db = new DatabaseContext(settings);
-			accountService = new AccountService(db, settings);
-			statisticsService = new StatisticsService(statsLogger, db);
-			cloudStorageService = new CloudStorageService(db, cloudStorageLogger);
-			clientService = new ClientService(db);
-			ratingsService = new RatingsService(ratingsLogger, db);
-		}
-
-		public async Task StartAsync(CancellationToken cancellationToken)
-		{
-			logger.LogInformation("Configuring MongoDB indexes.");
-			await accountService.CreateIndexesAsync();
-			await statisticsService.CreateIndexesAsync();
-			await ratingsService.CreateIndexesAsync();
-
-			logger.LogInformation("Initializing MongoDB CloudStorage.");
-			await cloudStorageService.EnsureSystemFilesExistAsync();
-
-			logger.LogInformation("Initializing MongoDB Clients.");
-			await clientService.UpdateDefaultClientsAsync();
-		}
-
-		public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+		this.logger = logger;
+		this.serviceProvider = serviceProvider;
 	}
+
+	public async Task StartAsync(CancellationToken cancellationToken)
+	{
+		using var scope = serviceProvider.CreateScope();
+
+		var accountService = scope.ServiceProvider.GetRequiredService<AccountService>();
+		var statisticsService = scope.ServiceProvider.GetRequiredService<StatisticsService>();
+		var ratingsService = scope.ServiceProvider.GetRequiredService<RatingsService>();
+		var cloudStorageService = scope.ServiceProvider.GetRequiredService<CloudStorageService>();
+		var clientService = scope.ServiceProvider.GetRequiredService<ClientService>();
+
+		logger.LogInformation("Configuring MongoDB indexes.");
+		await accountService.CreateIndexesAsync();
+		await statisticsService.CreateIndexesAsync();
+		await ratingsService.CreateIndexesAsync();
+
+		logger.LogInformation("Initializing MongoDB CloudStorage.");
+		await cloudStorageService.EnsureSystemFilesExistAsync();
+
+		logger.LogInformation("Initializing MongoDB Clients.");
+		await clientService.UpdateDefaultClientsAsync();
+	}
+
+	public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 }
