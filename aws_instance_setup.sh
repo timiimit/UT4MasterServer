d6089849c02cbb8ea8654c6478e3f35eaba9c82e
timiimit <timi.korda@gmail.com>
2023-02-18 13:30:12 +0100

    Improve deployment script

--- aws_instance_setup.sh
+++ aws_instance_setup.sh
@@ -13,6 +13,10 @@ exit
 #  - upload the file to target host
 #  - run 'tic terminfo' on the host in the same directory as the file resides
 
+WEBSITE_DOMAIN_NAME=
+API_DOMAIN_NAME=
+CERTIFICATE_REGISTRATION_EMAIL=
+
 # everything will be stored in /app
 # optionally mount a separate volume here
 sudo mkdir /app
@@ -25,7 +29,7 @@ sudo yum install -y git docker httpd mod_ssl
 
 # download and install docker-compose manually from their github
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.15.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-sudo chown +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # make docker-compose command available
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
@@ -40,26 +44,33 @@ sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.
 sudo yum install -y certbot python2-certbot-apache
 
 # now create ut4masterserver.conf in /etc/httpd/conf.d/ and put in the following:
-# <VirtualHost *:80>
-# 	DocumentRoot "/var/www/html"
-# 	ServerName "<PUT_WEBSITE_DOMAIN_NAME_HERE>"
-#	ProxyPreserveHost On
-#	ProxyPass / http://127.0.0.1:5001/
-#	ProxyPassReverse / http://127.0.0.1:5001/
-# </VirtualHost>
-# <VirtualHost *:80>
-# 	ServerName "<PUT_API_DOMAIN_NAME_HERE>"
-#	ProxyPreserveHost On
-#	ProxyPass / http://127.0.0.1:5000/
-#	ProxyPassReverse / http://127.0.0.1:5000/
-# </VirtualHost>
+cat >ut4masterserver.conf << EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html"
+    ServerName "$WEBSITE_DOMAIN_NAME"
+    ProxyPreserveHost On
+    ProxyPass / http://127.0.0.1:5001/
+    ProxyPassReverse / http://127.0.0.1:5001/
+</VirtualHost>
+<VirtualHost *:80>
+    ServerName "$API_DOMAIN_NAME"
+    ProxyPreserveHost On
+    ProxyPass / http://127.0.0.1:5000/
+    ProxyPassReverse / http://127.0.0.1:5000/
+</VirtualHost>
+EOF
+sudo cp ut4masterserver.conf /etc/httpd/conf.d/ut4masterserver.conf
+rm ut4masterserver.conf
 
 # make sure httpd (apache) daemon is running at all times
 sudo systemctl enable httpd & sudo systemctl restart httpd
 
 # install 2 ssl certificates. one for WEBSITE domain and one for API domain.
 # run certbot and follow it's instructions, then make sure it installed the obtained certificates.
-sudo certbot
+#sudo certbot
+
+# you can run this automated command instead of manually entering fields
+sudo certbot -d $API_DOMAIN_NAME -d $WEBSITE_DOMAIN_NAME --email $CERTIFICATE_REGISTRATION_EMAIL --non-interactive --apache --agree-tos
 
 # get source code of master server (use your own fork if you have a custom version)
 git clone https://github.com/timiimit/UT4MasterServer
